d7a529ac99a46638f5a984921117b2ad7b274062
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2015-10-09 08:11:27 +0100

    added caching

--- lib/bitcoin-client/api.rb
+++ lib/bitcoin-client/api.rb
@@ -1,7 +1,7 @@
 class BitcoinClient::API
   attr_reader :options
   attr_reader :params
-  
+
   def user; options[:user]; end
   def pass; options[:pass]; end
   def host; options[:host]; end
@@ -16,18 +16,35 @@ class BitcoinClient::API
 
   def initialize(options = {})
     @options = {
-      :host => 'localhost',
-      :port => 8332,
-      :ssl  => false
+      host:  'localhost',
+      port:  8332,
+      ssl:   false,
+      cache: false,
     }.merge(options)
+
+    if @options[:cache]
+      @redis = Redis.new
+    end
   end
-  
+
   def to_hash
     @options.dup
   end
-  
+
   def request(service_name, *params)
-    req = BitcoinClient::Request.new(service_name, params)
-    BitcoinClient::RPC.new(to_hash).dispatch(req)
+    if options[:cache]
+      cache_key = ([service_name] + params).join "_"
+    end
+
+    if options[:cache] && @redis.exists(cache_key)
+      # puts "FROM CACHE: #{cache_key}" # TODO: debug
+      @redis[cache_key]
+    else
+      req  = BitcoinClient::Request.new(service_name, params)
+      resp = BitcoinClient::RPC.new(to_hash).dispatch(req)
+      @redis.setex cache_key, 30, resp
+      resp
+    end
   end
+
 end
